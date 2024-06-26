72d387995d92849b0c27c026f93a4e3977a6a239
Miguel Correa <mctesterwork@gmail.com>
2024-06-25 19:34:52 -0500

    cambios a la migracion inicial para compatibilidad con cockroach db

--- entrenador-habitos/prisma/migrations/20240625000634_add_password_to_users/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `password` to the `USERS` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "USERS" ADD COLUMN     "password" TEXT NOT NULL;
