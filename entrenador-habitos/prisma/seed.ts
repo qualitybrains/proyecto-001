eb819d18344187b46cacbc0be0727ea6274a0b7a
mctesterwork <mctesterwork@gmail.com>
2024-07-02 21:10:42 -0500

    added a check before adding users to the db

--- entrenador-habitos/prisma/seed.ts
+++ entrenador-habitos/prisma/seed.ts
@@ -1,18 +1,24 @@
 import { db } from "../src/lib/db"
 
 async function main() {
-  await db.uSERS.createMany({
-    data: [{
-      full_name: 'Alice',
-      email: 'alice@prisma.io',
-      password: 'SimplePassword123'
-      },
-      {
-        full_name: 'Bob',
-        email: 'bob@prisma.io',
+  const user1 = await db.uSERS.findMany({where: {email: 'alice@prisma.io'}})
+  const user2 = await db.uSERS.findMany({where: {email: 'bob@prisma.io'}})
+  if (user1.length <= 0 && user2.length <= 0) {
+    await db.uSERS.createMany({
+      data: [{
+        full_name: 'Alice',
+        email: 'alice@prisma.io',
         password: 'SimplePassword123'
-      }]
-    })
+        },
+        {
+          full_name: 'Bob',
+          email: 'bob@prisma.io',
+          password: 'SimplePassword123'
+        }]
+      })
+    return
+  }
+  
 }
 
 main()
