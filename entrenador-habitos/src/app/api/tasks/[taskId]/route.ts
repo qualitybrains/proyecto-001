d17ece9f5346a085539bb937e64b56d2f29be218
mctesterwork <mctesterwork@gmail.com>
2024-07-17 19:12:34 -0500

    fix incorrect function type for delete route

--- entrenador-habitos/src/app/api/tasks/[taskId]/route.ts
+++ entrenador-habitos/src/app/api/tasks/[taskId]/route.ts
@@ -1,12 +1,13 @@
 import { getServerSession } from "next-auth/next";
 import { deleteTask } from "../../controllers/tasks";
 import { authOptions } from "@/lib/authOptions";
+import { NextResponse, NextRequest } from "next/server";
 
-export async function DELETE({ params }: { params: { taskId: string } }) {
+export async function DELETE(request: NextRequest, { params }: { params: { taskId: string } }) {
     const session = await getServerSession(authOptions);
     // If we don't have a session or user doesn't have email, return unauthorized
     if (!session || !session.user?.email) {
-        return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        return new NextResponse(JSON.stringify({ message: "Unauthorized" }), {
             status: 401,
             headers: {
                 "Content-Type": "application/json",
@@ -16,7 +17,7 @@ export async function DELETE({ params }: { params: { taskId: string } }) {
 
     const { taskId } = params;
     const task = await deleteTask(taskId);
-    return new Response(JSON.stringify(task), {
+    return new NextResponse(JSON.stringify(task), {
         status: 200,
         headers: {
             "Content-Type": "application/json",
