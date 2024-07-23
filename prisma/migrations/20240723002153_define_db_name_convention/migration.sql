8c4ed5c6be0699647908ceb99d82d5fd49685145
mctesterwork <mctesterwork@gmail.com>
2024-07-23 10:47:32 -0500

    unified and updated migrations

--- prisma/migrations/20240723002153_define_db_name_convention/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `full_name` on the `Users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Users" DROP COLUMN "full_name";
-ALTER TABLE "Users" ADD COLUMN     "fullName" STRING;
